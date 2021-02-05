70df5ea704b85e3586da099811924aed3d996af6
NazarineWasonga <nazarinewasonga48@gmail.com>
2021-02-05 16:35:25 +0300

    edited styles.css

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,13 +8,13 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote(1,`The greatest glory in living lies not in never falling, but in rising every time we fall.`,`Nelson`,`Nelson Mandela`, new Date(2021,1,7), 0,0),
+    new Quote(1,`"The greatest glory in living lies not in never falling, but in rising every time we fall."`,`Nelson`,`Nelson Mandela`, new Date(2021,1,7), 0,0),
 
-    new Quote(2,`If you look at what you have in life, you will always have more.If you look at what you do not have in life, you will never have enough.`,`Oprah`,`Oprah Winfrey`, new Date(2021,1,29),0,0),
+    new Quote(2,`"If you look at what you have in life, you will always have more.If you look at what you do not have in life, you will never have enough."`,`Oprah`,`Oprah Winfrey`, new Date(2021,1,29),0,0),
 
-    new Quote(3,`Life is what happens when you are busy making other plans.`,`John`,`John Lennon`,new Date(2021,1,9),0,0),
+    new Quote(3,`"Life is what happens when you are busy making other plans."`,`John`,`John Lennon`,new Date(2021,1,9),0,0),
  
-    new Quote(4, `No one is perfect - that’s why pencils have erasers.`, `Nazryn`, `Doryne boit`, new Date(2021, 1, 19 ),0,0),
+    new Quote(4, `"No one is perfect - that’s why pencils have erasers."`, `Nazryn`, `Doryne boit`, new Date(2021, 1, 19 ),0,0),
 ];
   addNewQuote(quote) {
     let quoteLength = this.quotes.length;
